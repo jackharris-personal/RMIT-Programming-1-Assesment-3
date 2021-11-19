d2a709291474b877c65d1870ae2b66732c26f5c6
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-19 11:06:28 +1100

    - Migrated comment elements out of the DashboardView and into the main View class. - DashboardView now extends the parent View.

--- src/com/jackgharris/rmit/cosc2135/views/DashboardView.java
+++ src/com/jackgharris/rmit/cosc2135/views/DashboardView.java
@@ -6,20 +6,10 @@ package com.jackgharris.rmit.cosc2135.views;
 import com.jackgharris.rmit.cosc2135.core.CustomArray;
 import com.jackgharris.rmit.cosc2135.core.TextColors;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 //**** START CLASS ****\\
-public class DashboardView{
-
-    //declare our buffered reader variable
-    BufferedReader br;
-
-    public DashboardView(){
-        //initialize our buffered reader variable to a new instance of the BufferedReader with a InputSteamReader and system in parsed
-        this.br = new BufferedReader(new InputStreamReader(System.in));
-    }
+public class DashboardView extends View{
 
     //**** HOME VIEW ****\\
     //This is the home page once the user is logged in
@@ -251,23 +241,4 @@ public class DashboardView{
 
     }
 
-    //**** SHOW ALERT HELPER METHOD ****\\
-    //Accepts a string to display and the textColor of the alert
-    private void showAlert(String alert, String textColor){
-        //boarder + textcolor appended
-        System.out.println(textColor+"_____________________________________\n");
-        //display alert
-        System.out.println("Alert: "+alert);
-        //end boarder and reset colors appended
-        System.out.println("_____________________________________\n"+TextColors.reset);
-    }
-
-    //**** SHOW TITLE HELPER METHOD ****\\
-    //Accepts the view title in string form
-    private void showTitle(String title){
-        //display the blue border indicating a new view has loaded
-        System.out.println(TextColors.backgroundBlue+"\n"+TextColors.reset);
-        //print the title as provided by a sting
-        System.out.println(TextColors.textBlueBold+title+"\n"+TextColors.reset);
-    }
 }
