a8655a27e701705552b23ba2d7e27aa4ed3d3859
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-21 11:45:06 +1100

    - Updated package name - Created Controllers and Models array - Updated constrctor to use the new array

--- src/com/jackgharris/rmit/cosc2135/assignment3/core/WhatsAppConsoleEdition.java
+++ src/com/jackgharris/rmit/cosc2135/assignment3/core/WhatsAppConsoleEdition.java
@@ -1,11 +1,15 @@
 //**** SET PACKAGE ****\\
-package com.jackgharris.rmit.cosc2135.core;
+package com.jackgharris.rmit.cosc2135.assignment3.core;
 
 //**** IMPORT PACKAGES ****\\
 //Here we import all the relevant packages that we will be referencing, calling and accessing in this class.
-import com.jackgharris.rmit.cosc2135.controllers.*;
-import com.jackgharris.rmit.cosc2135.exceptions.InvalidViewException;
-import com.jackgharris.rmit.cosc2135.models.*;
+import com.jackgharris.rmit.cosc2135.assignment3.controllers.*;
+import com.jackgharris.rmit.cosc2135.assignment3.exceptions.InvalidViewException;
+import com.jackgharris.rmit.cosc2135.assignment3.models.MessageModel;
+import com.jackgharris.rmit.cosc2135.assignment3.models.Model;
+import com.jackgharris.rmit.cosc2135.assignment3.models.User;
+import com.jackgharris.rmit.cosc2135.assignment3.models.UserModel;
+
 
 //**** START CLASS ****\\
 public class WhatsAppConsoleEdition {
@@ -18,18 +22,25 @@ public class WhatsAppConsoleEdition {
     private User user;
     //declare our controllers list
     private final CustomArray controllers;
+    //declare our models list
+    private final CustomArray models;
+
 
     //**** CONSTRUCTOR ****\\
     public WhatsAppConsoleEdition(){
-        //Models are done this way so we only have single instances of a model in the event two controllers both need the same model
-        //create our single instances of our models these are parsed to the controllers if needed in there constructors
-        UserModel userModel = new UserModel();
-        MessageModel messageModelModel = new MessageModel();
+
+        //Models are created in an array and then we can parse the whole array or a number of model from it to the controllers to use.
+        this.models = new CustomArray(Model.class);
+        //Create and add our user model to the array
+        this.models.add(new UserModel(),"userModel");
+        this.models.add(new MessageModel(),"messageModel");
 
         //Declare our list of controller instances and add them to the controllers array
         this.controllers = new CustomArray(Controller.class);
-        this.controllers.add(new LoginController(this,userModel),"login");
-        this.controllers.add(new DashboardController(this,userModel,messageModelModel), "dashboard");
+        this.controllers.add(new LoginController(this),"login");
+        this.controllers.add(new DashboardController(this), "dashboard");
+        this.controllers.add(new ErrorController(this),"error");
+        this.controllers.add(new AdminController(this), "admin");
 
         //set the active controller for this application to the login screen. (default start controller)
         this.setActiveController("login");
@@ -39,13 +50,20 @@ public class WhatsAppConsoleEdition {
     //**** SET ACTIVE CONTROLLER ****\\
     //sets the active controller based on a string
     public void setActiveController(String key){
+        //check if our array key exists for that controller
         if(this.controllers.arrayKeyExists(key)) {
+            //if so set the array key
             this.activeController = key;
         }else{
-            this.activeController = "login";
+            //else set the controller to the error controller
+            this.activeController = "error";
+            //create our response array
             CustomArray response = new CustomArray(String.class);
+            //set the redirect to the 500 view
+            response.add("500","redirect");
+            //add the error message and include the controller key that failed
             response.add("Error 500! Failed load controller "+key+"\nredirecting you back to the login","error");
-            response.add("welcome","redirect");
+            //finally recall our update view method
             this.updateView(response);
         }
     }
@@ -65,11 +83,14 @@ public class WhatsAppConsoleEdition {
             //save the view we attempted to load
             String targetView = ((Controller) this.controllers.getValue(this.activeController)).getCurrentView();
             //add the error to the response to be displayed to the user
-            response.add("Error 500! Were sorry the requested sub view "+targetView+"\ncould not be found in controller "+this.activeController,"error");
+            response.add("We're sorry the requested sub view "+targetView+"\ncould not be found in controller "+this.activeController,"error");
             //now add a redirect to the response
-            response.add("welcome","redirect");
+            response.add("404","redirect");
+            //finally we add a target reload for the view and controller
+            response.add(targetView,"targetView");
+            response.add(this.activeController,"targetController");
             //lastly we set our active controller back to the welcome view
-            this.activeController = "login";
+            this.activeController = "error";
             //finally we recall this update function
             this.updateView(response);
         }
@@ -85,6 +106,31 @@ public class WhatsAppConsoleEdition {
     //this method returns the current user from the this.user private variable
     public User getCurrentUser(){
         return this.user;
+    }
+
+    //**** GET MODEL ****\\
+    public Model getModel(String key){
+        //initialize our outcome
+        Model outcome = null;
+
+        //check if the model key exists in the array of models
+        if(this.models.arrayKeyExists(key)){
+            //if so then set the outcome to that model
+            outcome = (Model) this.models.getValue(key);
+        }else{
+            //else we create a response
+            CustomArray response = new CustomArray(String.class);
+            //add the fatal error message to the response
+            response.add("Fatal Error: Unknown model called "+key+"\nPlease contact support","error");
+            response.add("welcome","redirect");
+            //set the active controller to error
+            this.activeController = "login";
+            //re update our view
+            this.updateView(response);
+        }
+
+        //return the outcome
+        return outcome;
     }
 
     //**** MAIN STATIC VOID ****\\
