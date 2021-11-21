94c1ff7e023f6fa7839b83fa99e377d67ece0deb
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-21 11:47:39 +1100

    - Updated Justification

--- src/com/jackgharris/rmit/cosc2135/assignment3/exceptions/InvalidFullnameException.java
+++ src/com/jackgharris/rmit/cosc2135/assignment3/exceptions/InvalidFullnameException.java
@@ -1,7 +1,11 @@
+//**** SET PACKAGE ****\\
 package com.jackgharris.rmit.cosc2135.assignment3.exceptions;
 
+//**** START CLASSS ****\\
 public class InvalidFullnameException extends Exception{
+    //Invalid full name exception extends the exception class and is thrown by the registration model if the full name check fails
     public InvalidFullnameException(){
+        //set the main message for the exception to throw
         super("Invalid fullname provided!\nFullnames must be not blank");
     }
 }
