bf1d7f77e915420cf4fa3aefcbcaf2ac5cd769d9
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-11-21 11:50:21 +1100

    - Updated Justification

--- src/com/jackgharris/rmit/cosc2135/assignment3/exceptions/InvalidPasswordException.java
+++ src/com/jackgharris/rmit/cosc2135/assignment3/exceptions/InvalidPasswordException.java
@@ -1,8 +1,11 @@
+//**** SET PACKAGE ****\\
 package com.jackgharris.rmit.cosc2135.assignment3.exceptions;
 
+//**** START CLASSS ****\\
 public class InvalidPasswordException extends Exception{
-
+    //invalid password exception extends the exception and is thrown by the UserModel if the password check fails
     public InvalidPasswordException(){
+        //set the exception message that needs to be thrown
         super("Invalid password provided!\npasswords must not be blank");
     }
 }
--- src/com/jackgharris/rmit/cosc2135/assignment3/exceptions/InvalidViewException.java
+++ src/com/jackgharris/rmit/cosc2135/assignment3/exceptions/InvalidViewException.java
@@ -1,7 +1,11 @@
+//**** SET PACKAGE ****\\
 package com.jackgharris.rmit.cosc2135.assignment3.exceptions;
 
+//**** START CLASSS ****\\
 public class InvalidViewException extends Exception{
+    //invalid view exception is thrown by the update view class if a redirect calls a invalid view
     public InvalidViewException(){
+        //set the message to be caught and displayed
         super("Unknown view method called");
     }
 }
